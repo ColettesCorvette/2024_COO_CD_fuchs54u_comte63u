ee5080ad85f8a17128dda5472d872f945c71f6a6
Baloo54 <gabriel.comte2006@gmail.com>
2024-05-14 11:49:03 +0200

    chore: Update sorting methods in CD and Magasin classes, remove commented code in MainChargeurMagasin.java, and update file path for musicbrainzSimple

--- main/MainChargeurMagasin.java
+++ main/MainChargeurMagasin.java
@@ -29,10 +29,9 @@ public class MainChargeurMagasin {
 		sc.nextLine();
 		sc.close();
 		resultat.trierAriste();
-		System.out.println(resultat);
-
-		resultat.trierAlbum();
 		//System.out.println(resultat);
+		resultat.trierAlbum();
+		System.out.println(resultat);
 	}
 
 }
