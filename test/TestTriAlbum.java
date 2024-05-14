e0f07b668ea135991ece53fa0da835389df65e27
Thomas <th.fchs@gmail.com>
2024-05-14 11:49:31 +0200

    oof

--- test/TestTriAlbum.java
+++ test/TestTriAlbum.java
@@ -1,16 +1,28 @@
 package test;
 
+import XML.ChargeurMagasin;
+import donnees.CD;
 import donnees.Magasin;
 import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestTriAlbum {
 
     @Test
-    public void testTri()
-    {
-        Magasin m = new Magasin();
+    public void testTriAlbum() throws FileNotFoundException {
+
+        String repertoire = "musicbrainzSimple/";
+        ChargeurMagasin charge = new ChargeurMagasin(repertoire);
+        Magasin resultat = charge.chargerMagasin();
+
+        resultat.trierAlbum();
+
+        assertEquals(resultat.getCd(0).getNomArtiste(),"Believe");
+        assertEquals(resultat.getCd(1).getNomArtiste(), "Bénabar");
+        assertEquals(resultat.getCd(2).getNomArtiste(), "Demon Days");
 
     }
 
